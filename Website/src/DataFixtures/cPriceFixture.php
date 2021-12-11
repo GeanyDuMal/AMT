d38fca82a37f234ba714615842ae516503bb9de8
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-11 23:13:58 +0100

    Create cPriceFixture.php

---
+++ Website/src/DataFixtures/cPriceFixture.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ClientType;
+use App\Entity\Price;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class cPriceFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $clientTypeRepository = $manager->getRepository(ClientType::class);
+        $productRepository = $manager->getRepository(Product::class);
+
+        $allProduct = $productRepository->findAll();
+        $allClient = $clientTypeRepository->findAll();
+
+        $price1 = new Price();
+        $price1->setClientType($allClient[0])
+                ->setProduct($allProduct[0])
+                ->setPrice(0.6);
+        $manager->persist($price1);
+
+        $price2 = new Price();
+        $price2->setClientType($allClient[1])
+                ->setProduct($allProduct[0])
+                ->setPrice(0.8);
+        $manager->persist($price2);
+
+        $price3 = new Price();
+        $price3->setClientType($allClient[0])
+                ->setProduct($allProduct[1])
+                ->setPrice(0.6);
+        $manager->persist($price3);
+
+        $price4 = new Price();
+        $price4->setClientType($allClient[1])
+                ->setProduct($allProduct[1])
+                ->setPrice(0.8);
+        $manager->persist($price4);
+
+        $manager->flush();
+    }
+}
