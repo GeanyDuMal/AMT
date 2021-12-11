8c1d3b1de1683f50925a58f0d5959bd548e6c354
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-12 00:36:55 +0100

    Create AssociationFixture.php

---
+++ Website/src/DataFixtures/AssociationFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Association;
+use App\Entity\AssociationRole;
+use App\Entity\Client;
+use App\Entity\ClientType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AssociationFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $clientRepository = $manager->getRepository(Client::class);
+        $clientTypeRepository = $manager->getRepository(ClientType::class);
+        $associationRoleRepository = $manager->getRepository(AssociationRole::class);
+
+        $membreAssoc = $clientRepository->findBy(["clientType" => $clientTypeRepository->findOneBy(["name" => "Association"])]);
+
+        $association1 = new Association();
+        $association1->setMember($membreAssoc[0])
+                    ->setRole($associationRoleRepository->findOneBy(["name" => "President"]));
+        $manager->persist($association1);
+
+        $association2 = new Association();
+        $association2->setMember($membreAssoc[1])
+                    ->setRole($associationRoleRepository->findOneBy(["name" => "Membre"]));
+        $manager->persist($association2);
+        
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return[
+            ClientFixture::class,
+            AssociationRoleFixture::class
+        ];
+    }
+}
