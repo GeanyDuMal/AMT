eda944130d47fafd699b8152d93791419fc48b5b
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-12 00:37:18 +0100

    Create PostTypeFixture.php

---
+++ Website/src/DataFixtures/PostTypeFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\PostType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PostTypeFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $postType1 = new PostType();
+        $postType1->setName("Event");
+        $manager->persist($postType1);
+
+        $postType2 = new PostType();
+        $postType2->setName("Autre");
+        $manager->persist($postType2);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return NULL;
+    }
+}
