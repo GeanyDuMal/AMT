13fb61d60cb16693da2af4f31cce5aaebdf4c836
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-11 23:13:48 +0100

    Create aProductTypeFixture.php

---
+++ Website/src/DataFixtures/aProductTypeFixture.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ProductType;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class aProductTypeFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $productType1 = new ProductType();
+        $productType1->setName("Snack");
+        $manager->persist($productType1);
+
+        $productType2 = new ProductType();
+        $productType2->setName("Boisson");
+        $manager->persist($productType2);
+
+        $manager->flush();
+    }
+}
