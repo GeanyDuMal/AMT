69f3cef88c1b9a14106a95bd18a71ace82766844
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-12 00:37:06 +0100

    Create ClientTypeFixture.php

---
+++ Website/src/DataFixtures/ClientTypeFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ClientType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ClientTypeFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $clientType1 = new ClientType();
+        $clientType1->setName("Association");
+        $manager->persist($clientType1);
+        
+        $clientType2 = new ClientType();
+        $clientType2->setName("Etudiant");
+        $manager->persist($clientType2);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return NULL;
+    }
+}
