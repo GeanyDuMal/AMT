472fddcc1cb57ce44e5e3f21f49172506e0ecfed
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-12 00:37:13 +0100

    Create PaymentTypeFixture.php

---
+++ Website/src/DataFixtures/PaymentTypeFixture.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\PaymentType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PaymentTypeFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $paymentType1 = new PaymentType();
+        $paymentType1->setName("Carte Bancaire");
+        $manager->persist($paymentType1);
+
+        $paymentType2 = new PaymentType();
+        $paymentType2->setName("Espece");
+        $manager->persist($paymentType2);
+
+        $paymentType3 = new PaymentType();
+        $paymentType3->setName("Solde");
+        $manager->persist($paymentType3);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return NULL;
+    }
+}
