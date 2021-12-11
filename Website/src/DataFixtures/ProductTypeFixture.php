b6ed15bed67d6723a6382456304daa0b4ffd227b
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-12 00:39:05 +0100

    Create PurchaseFixture.php

---
+++ Website/src/DataFixtures/ProductTypeFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ProductType;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ProductTypeFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $productType1 = new ProductType();
+        $productType1->setName("Snack");
+        $manager->persist($productType1);
+
+        $productType2 = new ProductType();
+        $productType2->setName("Boisson");
+        $manager->persist($productType2);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return NULL;
+    }
+}
