82724eae126df9c68333a292412cf8839ef81879
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-13 23:15:36 +0100

    Update SignInController.php

--- Website/src/Controller/SignInController.php
+++ Website/src/Controller/SignInController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +16,11 @@ class SignInController extends AbstractController
     /**
      * @Route("/sign_in", name="sign_in")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
         $client = new Client;
 
+        $flush = false;
         $formNewClient = $this->createFormBuilder($client)
                             ->add("name")
                             ->add("firstName")
@@ -24,8 +28,19 @@ class SignInController extends AbstractController
                             ->add("password", PasswordType::class)
                             ->getForm();
 
+        $formNewClient->handleRequest($request);
+
+        if($formNewClient->isSubmitted() && $formNewClient->isValid()){
+            $manager->persist($client);
+            $manager->flush();
+            $flush = true;
+
+        }
+
+
         return $this->render('connexion/sign_in/index.html.twig', [
             'formCreationClient' => $formNewClient->createView(),
+            'flush' => $flush
         ]);
     }
 }
