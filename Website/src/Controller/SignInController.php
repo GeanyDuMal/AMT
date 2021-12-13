827bc82226152cfce9a054751e8b7f0f166ac643
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-13 23:36:07 +0100

    Update SignInController.php

--- Website/src/Controller/SignInController.php
+++ Website/src/Controller/SignInController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Client;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -19,8 +18,9 @@ class SignInController extends AbstractController
     public function index(Request $request, EntityManagerInterface $manager): Response
     {
         $client = new Client;
-
+        $clientRepository = $manager->getRepository(Client::class);
         $flush = false;
+
         $formNewClient = $this->createFormBuilder($client)
                             ->add("name")
                             ->add("firstName")
@@ -30,11 +30,20 @@ class SignInController extends AbstractController
 
         $formNewClient->handleRequest($request);
 
-        if($formNewClient->isSubmitted() && $formNewClient->isValid()){
+        //Verif duplicata
+        $data = $formNewClient->getData();
+
+        $duplicata = $clientRepository->findOneBy(["login" => $data->getLogin()]);
+        dump($duplicata);
+
+        //Faire plus de verif et test que login n'existe pas 
+        //Message dans le cas ou il y a une erreur
+        if($formNewClient->isSubmitted() && $formNewClient->isValid() && $duplicata == NULL){
             $manager->persist($client);
             $manager->flush();
             $flush = true;
 
+            //Redirection ? 
         }
 
 
