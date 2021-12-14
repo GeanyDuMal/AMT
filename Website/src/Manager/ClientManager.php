0947ded80019cca33b9a7dcb144976b4d88c49f9
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-15 00:16:34 +0100

    Ajout regle metier + commentaire

--- Website/src/Manager/ClientManager.php
+++ Website/src/Manager/ClientManager.php
@@ -14,6 +14,11 @@ class ClientManager
           $this->manager = $managerController;
       }
 
+    /**
+     * @param Client|null $client
+     * @return bool
+     * Check if the differents attributes aren't empty
+     */
       public function isEmpty(?Client $client): bool
       {
             if ($client->getName() == "" && $client->getFirstname() == "" && $client->getLogin() == "" && $client->getPassword() == ""){
@@ -24,6 +29,11 @@ class ClientManager
             }
       }
 
+    /**
+     * @param Client|null $client
+     * @return bool
+     * Check if the login is already assign to someone in the Database
+     */
       public function loginExists(?Client $client): bool
       {
           $clientRepository = $this->manager->getRepository(Client::class);
@@ -33,19 +43,41 @@ class ClientManager
           return !is_null($dupplicata);
       }
 
+    /**
+     * @param Client|null $client
+     * @return bool
+     * Allow to verify the data :
+     * Check if the password contains a special character
+     * Check if the different input are the right lenght
+     * Check if the name and first name doesn't contain a special character
+     */
       public function dataCorrect(?Client $client): bool
       {
-          $regexSpecial = "#$%^&*()+=-[]';,./{}|:<>?~";
-          $containsRegex = strpbrk($client->getPassword(), $regexSpecial);
+          if (!is_null($client)){
+              $regexSpecial = "#$%^&*()+=-[]';,./{}|:<>?~";
 
-          $nameUpperThree = (strlen($client->getName()) >=3);
-          $firstNameUpperThree = (strlen($client->getFirstName()) >=3);
-          $loginUpperFive = (strlen($client->getLogin()) >=5);
-          $passwordUpperFive = (strlen($client->getPassword()) >=5);
+              $containsSpecialPassword = strpbrk($client->getPassword(), $regexSpecial);
+              $containsSpecialName = strpbrk($client->getName(), $regexSpecial);
+              $containsSpecialFirstName = strpbrk($client->getFirstName(), $regexSpecial);
 
-          return ($containsRegex && $loginUpperFive && $passwordUpperFive & $firstNameUpperThree && $nameUpperThree);
+              $nameUpperThree = (strlen($client->getName()) >=3);
+              $firstNameUpperThree = (strlen($client->getFirstName()) >=3);
+              $loginUpperFive = (strlen($client->getLogin()) >=5);
+              $passwordUpperFive = (strlen($client->getPassword()) >=5);
+
+              return ($containsSpecialPassword && $loginUpperFive && $passwordUpperFive && $firstNameUpperThree
+                  && $nameUpperThree && !$containsSpecialName && !$containsSpecialFirstName);
+          }
+          else{
+              return false;
+          }
       }
 
+    /**
+     * @param Client|null $client
+     * @return void
+     * Insert the Client in the Database
+     */
       public function persist(?Client $client)
       {
           $this->manager->persist($client);
