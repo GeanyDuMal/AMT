3dcbcfbfdbc6044f58a4123fd1d4cd66db1f17a5
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-15 23:13:57 +0100

    Creation Js page Signin

--- Website/public/script/connexion/signin.js
+++ Website/public/script/connexion/signin.js
@@ -1,5 +1,36 @@
-function verifSaisie() {
-    var form = document.querySelector("#form_signin")
+function verifyInput() {
+    var form = document.querySelector("#form_signin");
+    var message = "";
 
-    valueLogin = form.name.value;
+    var valueName = form.name.value;
+    var valueFirstName = form.firstName.value;
+    var valueLogin = form.login.value;
+    var valuePassword = form.password.value;
+    var valueConfirm = form.confirmPassword.value;
+
+    if (valueName == ""){
+        message += "Nom non renseigné\n";
+    }
+    else if(valueName.length() < 3){
+        message += "Nom trop court\n";
+    }
+
+    if (condition) {
+        
+    }
+
+    if (condition) {
+        
+    }
+
+    if (condition) {
+        
+    }
+
+    if (condition) {
+        
+    }
+
+
+    return false;
 }
