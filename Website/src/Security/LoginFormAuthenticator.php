07df709d051cd4650675bf7d5e9487f39365499c
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-18 11:13:37 +0100

    edit connection

--- Website/src/Security/LoginFormAuthenticator.php
+++ Website/src/Security/LoginFormAuthenticator.php
@@ -14,11 +14,17 @@ use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationExc
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -34,6 +40,11 @@ class LoginFormAuthenticator extends AbstractAuthenticator
     private $urlGenerator;
     private $csrfTokenManager;
     private $passwordEncoder;
+    private $successHandler;
+    private $options;
+    private $httpUtils;
+    private $failureHandler;
+    private $userProvider;
 
     public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
     {
@@ -44,7 +55,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator
     }
 
 
-    public function getCredentials(Request $request)
+    public function getCredentials(Request $request): array
     {
         $credentials = [
             'username' => $request->request->get('username'),
@@ -87,23 +98,48 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         return $this->urlGenerator->generate('app_login');
     }
 
-    public function authenticate(Request $request)
+    public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        $credentials = $this->getCredentials($request);
+
+        // @deprecated since Symfony 5.3, change to $this->userProvider->loadUserByIdentifier() in 6.0
+        $method = 'loadUserByIdentifier';
+        if (!method_exists($this->userProvider, 'loadUserByIdentifier')) {
+            trigger_deprecation('symfony/security-core', '5.3', 'Not implementing method "loadUserByIdentifier()" in user provider "%s" is deprecated. This method will replace "loadUserByUsername()" in Symfony 6.0.', get_debug_type($this->userProvider));
+
+            $method = 'loadUserByUsername';
+        }
+
+        $passport = new Passport(
+            new UserBadge($credentials['username'], [$this->userProvider, $method]),
+            new PasswordCredentials($credentials['password']),
+            [new RememberMeBadge()]
+        );
+        if ($this->options['enable_csrf']) {
+            $passport->addBadge(new CsrfTokenBadge($this->options['csrf_token_id'], $credentials['csrf_token']));
+        }
+
+        if ($this->userProvider instanceof PasswordUpgraderInterface) {
+            $passport->addBadge(new PasswordUpgradeBadge($credentials['password'], $this->userProvider));
+        }
+
+        return $passport;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
-    }
-
-    public function supports(Request $request): ?bool
-    {
-        // TODO: Implement supports() method.
+        return $this->failureHandler->onAuthenticationFailure($request, $exception);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return $this->successHandler->onAuthenticationSuccess($request, $token);
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return (!$this->options['post_only'] || $request->isMethod('POST'))
+            && $this->httpUtils->checkRequestPath($request, $this->options['check_path'])
+            && (!$this->options['form_only'] || 'form' === $request->getContentType());
     }
 }
