0286c6ec5fa3e768d07253714c4402918694ab02
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-21 21:54:20 +0100

    modif migrations

--- Website/migrations/Version20211221201549.php
+++ Website/migrations/Version20211221201549.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211211221636 extends AbstractMigration
+final class Version20211221201549 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,7 @@ final class Version20211211221636 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE association (member_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_FD8521CCD60322AC (role_id), PRIMARY KEY(member_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE association_role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, client_type_id INT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL UNIQUE , password VARCHAR(255) NOT NULL, balance NUMERIC(5, 2) NULL DEFAULT "0", fidelity_point INT NULL DEFAULT "0", INDEX IDX_C74404559771C8EE (client_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, client_type_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, balance NUMERIC(5, 2) DEFAULT \'0\', fidelity_point INT DEFAULT 0, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', UNIQUE INDEX UNIQ_C7440455AA08CB10 (login), INDEX IDX_C74404559771C8EE (client_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE client_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE command (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, payment_type_id INT NOT NULL, ordered_at DATETIME NOT NULL, INDEX IDX_8ECAEAD419EB6921 (client_id), INDEX IDX_8ECAEAD4DC058279 (payment_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE payment_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -33,22 +33,22 @@ final class Version20211211221636 extends AbstractMigration
         $this->addSql('CREATE TABLE product_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE purchase (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, command_id INT NOT NULL, quantity INT NOT NULL, INDEX IDX_6117D13B4584665A (product_id), INDEX IDX_6117D13B33E1689A (command_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
-        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CC7597D3FE FOREIGN KEY (member_id) REFERENCES client (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CCD60322AC FOREIGN KEY (role_id) REFERENCES association_role (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD419EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD4DC058279 FOREIGN KEY (payment_type_id) REFERENCES payment_type (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF8A43BA0 FOREIGN KEY (post_type_id) REFERENCES post_type (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D94584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D99771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD14959723 FOREIGN KEY (product_type_id) REFERENCES product_type (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B4584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE RESTRICT ON UPDATE CASCADE');
-        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B33E1689A FOREIGN KEY (command_id) REFERENCES command (id) ON DELETE RESTRICT ON UPDATE CASCADE');
+        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CC7597D3FE FOREIGN KEY (member_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CCD60322AC FOREIGN KEY (role_id) REFERENCES association_role (id)');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id)');
+        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD419EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD4DC058279 FOREIGN KEY (payment_type_id) REFERENCES payment_type (id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF8A43BA0 FOREIGN KEY (post_type_id) REFERENCES post_type (id)');
+        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D94584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D99771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD14959723 FOREIGN KEY (product_type_id) REFERENCES product_type (id)');
+        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B33E1689A FOREIGN KEY (command_id) REFERENCES command (id)');
 
-    /* Creation des lignes references (Type)
-     * Peuvent etre commenté ici et decommenté dans les fixture
-     * Permet de créer les types obligatoires lors de l'importation de la base de données et pas uniquement avec le jeu de données de test
-     */
+        /* Creation des lignes references (Type)
+         * Peuvent etre commenté ici et decommenté dans les fixture
+         * Permet de créer les types obligatoires lors de l'importation de la base de données et pas uniquement avec le jeu de données de test
+         */
 
         //Type de Payement
         $this->addSql('INSERT INTO payment_type (name) VALUES ("Carte Bancaire");');
@@ -75,7 +75,7 @@ final class Version20211211221636 extends AbstractMigration
         $this->addSql('INSERT INTO product_type (name) VALUES ("Boisson");');
 
 
-    //Creation des triggers
+        //Creation des triggers
         //Trigger table Association
         $this->addSql('CREATE TRIGGER modifClientTypeAdd 
                             AFTER INSERT ON association FOR EACH ROW
@@ -111,7 +111,7 @@ final class Version20211211221636 extends AbstractMigration
                             END IF;');
 
         //Trigger table Client
-            //Sur ce trigger, setup de balance et fidelity a 0 car le default ne fonctionne pas avec $manager->flush()
+        //Sur ce trigger, setup de balance et fidelity a 0 car le default ne fonctionne pas avec $manager->flush()
         $this->addSql('CREATE TRIGGER verifCreationClient BEFORE INSERT ON client FOR EACH ROW
                             BEGIN
                                 IF(NEW.balance < 0 || ISNULL(NEW.balance)) THEN
