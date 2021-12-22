3924e1f5c19efa2884a6c1facf1e2c508fc03e3f
OmarEll <omarelloumi530@gmail.com>
2021-12-22 20:35:14 +0100

    client

---
+++ Website/migrations/Version20211221130514.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211221130514 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE association (member_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_FD8521CCD60322AC (role_id), PRIMARY KEY(member_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE association_role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, client_type_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, balance NUMERIC(5, 2) DEFAULT \'0\', fidelity_point INT DEFAULT 0, UNIQUE INDEX UNIQ_C7440455AA08CB10 (login), INDEX IDX_C74404559771C8EE (client_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE command (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, payment_type_id INT NOT NULL, ordered_at DATETIME NOT NULL, INDEX IDX_8ECAEAD419EB6921 (client_id), INDEX IDX_8ECAEAD4DC058279 (payment_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE payment_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, post_type_id INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, image_link VARCHAR(255) NOT NULL, INDEX IDX_5A8A6C8DF8A43BA0 (post_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE price (product_id INT NOT NULL, client_type_id INT NOT NULL, price NUMERIC(5, 2) NOT NULL, INDEX IDX_CAC822D94584665A (product_id), INDEX IDX_CAC822D99771C8EE (client_type_id), PRIMARY KEY(product_id, client_type_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product_type_id INT NOT NULL, name VARCHAR(255) NOT NULL, quantity_stock INT NOT NULL, image_link VARCHAR(255) NOT NULL, INDEX IDX_D34A04AD14959723 (product_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE purchase (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, command_id INT NOT NULL, quantity INT NOT NULL, INDEX IDX_6117D13B4584665A (product_id), INDEX IDX_6117D13B33E1689A (command_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CC7597D3FE FOREIGN KEY (member_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE association ADD CONSTRAINT FK_FD8521CCD60322AC FOREIGN KEY (role_id) REFERENCES association_role (id)');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id)');
+        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD419EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD4DC058279 FOREIGN KEY (payment_type_id) REFERENCES payment_type (id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF8A43BA0 FOREIGN KEY (post_type_id) REFERENCES post_type (id)');
+        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D94584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE price ADD CONSTRAINT FK_CAC822D99771C8EE FOREIGN KEY (client_type_id) REFERENCES client_type (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD14959723 FOREIGN KEY (product_type_id) REFERENCES product_type (id)');
+        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13B33E1689A FOREIGN KEY (command_id) REFERENCES command (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE association DROP FOREIGN KEY FK_FD8521CCD60322AC');
+        $this->addSql('ALTER TABLE association DROP FOREIGN KEY FK_FD8521CC7597D3FE');
+        $this->addSql('ALTER TABLE command DROP FOREIGN KEY FK_8ECAEAD419EB6921');
+        $this->addSql('ALTER TABLE client DROP FOREIGN KEY FK_C74404559771C8EE');
+        $this->addSql('ALTER TABLE price DROP FOREIGN KEY FK_CAC822D99771C8EE');
+        $this->addSql('ALTER TABLE purchase DROP FOREIGN KEY FK_6117D13B33E1689A');
+        $this->addSql('ALTER TABLE command DROP FOREIGN KEY FK_8ECAEAD4DC058279');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DF8A43BA0');
+        $this->addSql('ALTER TABLE price DROP FOREIGN KEY FK_CAC822D94584665A');
+        $this->addSql('ALTER TABLE purchase DROP FOREIGN KEY FK_6117D13B4584665A');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD14959723');
+        $this->addSql('DROP TABLE association');
+        $this->addSql('DROP TABLE association_role');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE client_type');
+        $this->addSql('DROP TABLE command');
+        $this->addSql('DROP TABLE payment_type');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE post_type');
+        $this->addSql('DROP TABLE price');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE product_type');
+        $this->addSql('DROP TABLE purchase');
+    }
+}
---
+++ Website/src/Controller/Client/ClientController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller\Client;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Client;
+use App\Entity\ClientType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Manager\ClientManager;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="client_list",methods={"GET", "POST"} )
+     */
+    public function index(): Response
+    {
+        $clients = $this->getDoctrine()->getRepository(Client::class)->findAll();
+        return $this->render('client/index.html.twig', array('clients' => $clients));
+    }
+    /**
+     * @Route("/client/new", name="new_client",methods={"GET", "POST"} )
+     */
+    public function addClientAction(Request $request, EntityManagerInterface $manager): Response
+    {
+        $data = $request->request;
+        $clientManager = new ClientManager($manager);
+        $client = new Client();
+        $error = "";
+        if ($data->count() > 0) {
+            $client->setName($data->get('name'));
+            $client->setFirstName($data->get('fname'));
+            $client->setLogin($data->get('login'));
+            $client->setPassword($data->get('pwd'));
+            if ($data->get('balance') == "")
+                $client->setBalance(0);
+            else
+                $client->setBalance($data->get('balance'));
+            if (!$clientManager->isNotFull($client)) {
+                $error = "Les champs ne doit pas etre null!";
+            } elseif (!$clientManager->dataCorrect($client)) {
+                $error = "Vérifier vos champs!";
+            } elseif ($clientManager->loginExists($client)) {
+                $error = "Ce login existe déja!";
+            } else if (strcmp($data->get('confPwd'), $client->getPassword()) != 0)
+                $error = "Vérifier la confirmation de votre mot de passe!";
+        }
+        $this->addFlash("error", $error);
+        $types = $this->getDoctrine()->getRepository(ClientType::class)->findAll();
+        return $this->render(
+            'client/AddModalClient.html.twig',
+            [
+                'types' => $types,
+                'client' => $client
+            ]
+        );
+    }
+    /**
+     * @Route("/client/edit/{id}", name="edit_client",methods={"GET", "POST"} )
+     */
+    public function editClientAction(): Response
+    {
+        $client = new Client();
+        return $this->render('client/EditModalClient.html.twig');
+    }
+    /**
+     * @Route("/client/delete/{id}", name="delete_client", methods="GET" )
+     */
+    public function deleteClientAction($id, EntityManagerInterface $manager): Response
+    {
+        $client = $manager->getRepository('App:Client')->find($id);
+        $manager->remove($client);
+        $manager->flush();
+        $this->addFlash('message', 'Client supprimer avec succée');
+        return $this->redirectToRoute('client_list');
+    }
+}
