b7a1f7cbb8fbbdd1f77c9effbc09643b3cb1922e
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-22 18:20:32 +0100

    JS

--- Website/public/script/connexion/signin.js
+++ Website/public/script/connexion/signin.js
@@ -10,7 +10,7 @@ childs.forEach(child => {
 
 function verifyInputSignin() {
 
-    let message = "";
+    let message = "<ul>";
     const regexCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
 
@@ -22,51 +22,53 @@ function verifyInputSignin() {
 
     form.name.style.borderColor = "red";
     if (valueName === ""){
-        message += "Nom non renseigné, ";
+        message += "<li>Nom non renseigné</li>";
     }else if(valueName.length < 3){
-        message += "Nom trop court, ";
+        message += "<li>Nom trop court</li>";
     }else{
         form.name.style.borderColor = "black";
     }
 
     form.firstName.style.borderColor = "red";
     if (valueFirstName === ""){
-        message += "Prenom non renseigné, ";
+        message += "<li>Prenom non renseigné</li>";
     }else if(valueFirstName.length < 3){
-        message += "Prenom trop court, ";
+        message += "<li>Prenom trop court</li>";
     }else{
         form.firstName.style.borderColor = "black";
     }
 
     form.login.style.borderColor = "red";
     if (valueLogin === ""){
-        message += "Login non renseigné, ";
+        message += "<li>Login non renseigné</li>";
     }else if(valueLogin.length < 5){
-        message += "Login trop court, ";
+        message += "<li>Login trop court</li>";
     }else{
         form.login.style.borderColor = "black";
     }
 
     form.password.style.borderColor = "red";
     if (valuePassword === ""){
-        message += "Mot de Passe non renseigné ";
+        message += "<li>Mot de Passe non renseigné</li>";
     }else if(valuePassword.length < 5){
-        message += "Mot de Passe trop court ";
+        message += "<li>Mot de Passe trop court</li>";
     }else if (regexCharacter.test(valuePassword) === false) {
-        message += "Le mots de passe ne contient pas de caractere spécial "
+        message += "<li>Le mots de passe ne contient pas de caractere spécial</li>"
     }
     else if (valuePassword !== valueConfirm) {
-        message += "Les mots de passe ne correspondent pas "
+        message += "<li>Les mots de passe ne correspondent pas</li>"
         form.confirmPassword.style.borderColor = "red";
     }else{
         form.password.style.borderColor = "black";
         form.confirmPassword.style.borderColor = "black";
     }
 
-    if (message !== "") {
+    message += "</ul>"
+
+    if (message !== "<ul></ul>") {
         Swal.fire({
             title: 'Incomplet !',
-            text: message,
+            html: message,
             icon: 'error',
             confirmButtonText: 'Completer'
           })
