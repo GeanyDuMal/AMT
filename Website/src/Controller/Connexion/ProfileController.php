fb08f3add7b422cb6b09b01740eaf3b3fe787364
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-22 17:58:48 +0100

    a

--- Website/src/Controller/Connexion/ProfileController.php
+++ Website/src/Controller/Connexion/ProfileController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Connexion;
 
 use App\Entity\Client;
+use App\Manager\ClientManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -14,15 +17,37 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile", name="profile")
      */
-    public function index(EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
     {
         if ($this->isGranted('IS_AUTHENTICATED_FULLY')){
             $clientRepository = $manager->getRepository(Client::class);
 
-            //dd(password_verify('kiki.', '$2y$13$yum1RBAT5kL12Am9jLRvxeaGoj2S4tSd8XBGvQKvTiuDMT3erPt7m'));
             //$this->isCsrfTokenValid()
 
             $client = $clientRepository->findOneBy(["login" => $this->getUser()->getUserIdentifier()]);
+
+            $inputParameterBag = $request->request;
+
+            // Verifie que les champs soit bien rempli
+            if (!is_null($inputParameterBag->get("oldPassword"))
+                && !is_null($inputParameterBag->get("newPassword"))
+                && trim($inputParameterBag->get("oldPassword")) === trim($inputParameterBag->get("newPassword")))
+            {
+                $clientManager = new ClientManager($manager);
+
+                // Verifie que l'ancien mot de passe corresponde et que le nouveau soit correct
+                if (password_verify(trim($inputParameterBag->get("oldPassword")), $this->getUser()->getPassword())
+                    && $clientManager->verifPassword(trim($inputParameterBag->get("oldPassword"))))
+                {
+                    $hashedPassword = $passwordHasher->hashPassword(
+                        $client,
+                        trim($inputParameterBag->get("newPassword")));
+
+                    $client->setPassword($hashedPassword);
+
+                    $clientManager->persist($client);
+                }
+            }
 
             return $this->render('connexion/profile/index.html.twig', [
                 "user" => $client,
