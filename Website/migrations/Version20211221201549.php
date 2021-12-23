1acd1dacbf566543698fd2f2692d271d3b07096f
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-24 00:01:44 +0100

    Update Version20211221201549.php

--- Website/migrations/Version20211221201549.php
+++ Website/migrations/Version20211221201549.php
@@ -14,7 +14,7 @@ final class Version20211221201549 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Creation de la base';
     }
 
     public function up(Schema $schema): void
