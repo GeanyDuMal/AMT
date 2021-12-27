a62211b3084908d02ae7dafb6b50f2669a7e57ed
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-27 16:25:21 +0100

    Create Order

--- Website/src/Controller/Command/CreateOrderController.php
+++ Website/src/Controller/Command/CreateOrderController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Command;
 
+use App\Entity\Client;
+use App\Entity\ClientType;
+use App\Entity\Price;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +17,7 @@ class CreateOrderController extends AbstractController
     /**
      * @Route("/command/create", name="commandCreate")
      */
-    public function index(EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
         if (!$this->isGranted('ROLE_ASSOC')){
             return $this->redirectToRoute('home');
@@ -21,19 +25,64 @@ class CreateOrderController extends AbstractController
 
         $user = $this->getUser();
         $productRepository = $manager->getRepository(Product::class);
+        $clientRepository = $manager->getRepository(Client::class);
+        $clientTypeRepository = $manager->getRepository(ClientType::class);
+        $priceRepository = $manager->getRepository(Price::class);
         $allProductPositiveStock = [];
+        $inputParameterBag = $request->request;
+        $productOrdered = [];
+        $quantityOrdered = [];
 
         $allProduct = $productRepository->findAll();
-
         foreach ($allProduct as $product){
             if ($product->getQuantityStock() >0){
                 $allProductPositiveStock[] = $product;
             }
         }
 
+        //Recupere toutes les quantités de produit selectionné
+        foreach ($allProductPositiveStock as $product){
+            $quantity = $inputParameterBag->get("quantity_product_" . $product->getId());
+            if ($quantity != 0 ){
+                  $productOrdered[] = $product;
+                  $quantityOrdered = $quantityOrdered + [$product->getId() => $quantity];
+            }
+        }
+
+        //Si l'on a commandé au moins 1 produits
+        if ($productOrdered){
+            $clientCommande = null;
+            $typeClient = $clientTypeRepository->findBy(["name" => "Etudiant"]);
+            $montant = 0;
+
+            if($inputParameterBag->get("client_commande") != null){
+                $clientCommande = $clientRepository->find($inputParameterBag->get("client_commande"));
+            }
+            if ($clientCommande){
+                $typeClient = $clientCommande->getClientType();
+            }
+            foreach ($productOrdered as $product){
+
+                $montant = $montant + $priceRepository->findOneBy(["product" => $product,
+                                        "clientType" => $typeClient])->getPrice()*$quantityOrdered[$product->getId()];
+            }
+
+            return $this->render('command/payement.html.twig', [
+                "product" => $productOrdered,
+                "quantity" => $quantityOrdered,
+                "montant" => $montant
+            ]);
+        }
+
+
+
+
+        $allClient = $clientRepository->findAll();
+
         return $this->render('command/create.html.twig', [
             "user" => $user,
-            "productList" => $allProductPositiveStock
+            "productList" => $allProductPositiveStock,
+            "clientList" => $allClient
         ]);
     }
 }
