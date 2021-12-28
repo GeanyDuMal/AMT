354305d4405d1d33eaf0bc26a747e4f706e69447
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-28 16:28:00 +0100

    rename

--- Website/src/Controller/Command/PaymentOrderController.php
+++ Website/src/Controller/Command/PaymentOrderController.php
@@ -21,7 +21,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandPaymentController extends AbstractController
 {
     /**
-     * @Route("/command/payment/{productOrderedSerialized}&{idClient}", name="commandPayment")
+     * @Route("/command/payment/{productOrderedSerialized}&{idClient}", name="orderPayment")
      */
     public function index($productOrderedSerialized, $idClient, EntityManagerInterface $manager, Request $request): Response
     {
