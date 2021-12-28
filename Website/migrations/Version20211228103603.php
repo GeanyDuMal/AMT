02b1bee2ca2a9285524972231094ce3c4939ab57
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-28 11:58:29 +0100

    New Migration
    Ajout du trigger permettant de convertir les points de fidelité en solde

---
+++ Website/migrations/Version20211228103603.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211228103603 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client CHANGE client_type_id client_type_id INT NOT NULL');
+        $this->addSql('DROP TRIGGER IF EXISTS `db_aedi`.`verifSoldePositif`;
+                            CREATE TRIGGER verifSoldeAndFidelity 
+                                BEFORE UPDATE ON client FOR EACH ROW
+                                BEGIN
+                                    IF(NEW.balance < 0) THEN
+                                        SIGNAL SQLSTATE "45000"
+                                        SET MESSAGE_TEXT = "Solde negatif, erreur Update";
+                                    END IF;
+                                    IF(NEW.fidelity_point >= 200)THEN
+                                        BEGIN
+                                            SET NEW.fidelity_point = NEW.fidelity_point - 200;
+                                            SET NEW.balance = NEW.balance + 0.8; 
+                                        END;
+                                    END IF;
+                                END;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client CHANGE client_type_id client_type_id INT DEFAULT NULL');
+        $this->addSql('DROP TRIGGER IF EXISTS `db_aedi`.`verifSoldeAndFidelity`;
+                            CREATE TRIGGER verifSoldePositif 
+                                BEFORE UPDATE ON client FOR EACH ROW
+                                BEGIN
+                                    IF(NEW.balance < 0) THEN
+                                        SIGNAL SQLSTATE "45000"
+                                        SET MESSAGE_TEXT = "Solde negatif, erreur Update";
+                                    END IF;
+                                END;');
+    }
+}
