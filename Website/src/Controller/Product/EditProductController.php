58602402f75de97b05155b60920bdc6f73231208
OmarEll <omarelloumi530@gmail.com>
2021-12-28 19:30:14 +0100

    smol err

--- Website/src/Controller/Product/EditProductController.php
+++ Website/src/Controller/Product/EditProductController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class EditProductController extends AbstractController
 {
