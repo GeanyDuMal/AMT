e3281b779a4617aff499452c5ac35aa985b3892a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-30 12:50:53 +0100

    Update menuCommand.js

--- Website/public/script/command/menuCommand.js
+++ Website/public/script/command/menuCommand.js
@@ -13,3 +13,37 @@ $(document).ready( function () {
         }
     );
 } );
+
+/**
+ * Permet de gerer les alertes lorsque l'on clique sur le bouton supprimer
+ * A mettre ensuite d'un un ajax.JS
+ */
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
+
+function warningDelete(){
+    swalWithBootstrapButtons.fire({
+        title: 'Supprilmer la commande',
+        text: "Etes vous sur de vouloir supprimer la commande",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Suopprimer',
+        cancelButtonText: 'Annuler',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Supprimé!',
+                'La commande a été supprimé',
+                'success'
+            )
+        }
+    })
+    return result;
+}
