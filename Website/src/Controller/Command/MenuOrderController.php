4fdd5c528a60c7e803be6f0e41020049fee41ec5
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-12-31 16:43:00 +0100

    Update MenuOrderController.php

--- Website/src/Controller/Command/MenuOrderController.php
+++ Website/src/Controller/Command/MenuOrderController.php
@@ -2,13 +2,10 @@
 
 namespace App\Controller\Command;
 
-use App\Entity\Client;
-use App\Entity\ClientType;
 use App\Entity\Command;
 use App\Manager\OrderManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
