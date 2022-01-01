b8f5ea112a5d99541dd0ceb8b735cadfe8bc8b40
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-02 00:36:30 +0100

    debut AJAX

---
+++ Website/public/script/command/ajaxMenuCommand.js
@@ -0,0 +1,35 @@
+/**
+ * To apply the plugin DataTables (he makes the table so cool :3 )
+ * pagination
+ * search bar
+ * sorting per column
+ */
+$(document).ready( function () {
+    $('#order_table').DataTable(
+        {
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json"
+            }
+        }
+    );
+} );
+
+
+/**
+ * delete a client/member using jquery-ajax
+ */
+$("#client_table").on("click", "#toDelete", function() {
+    var tr= $(this).closest("tr");
+    if (confirm('Voulez-vous supprimer l\'utilisateur ? ')) {
+        const id=$(this).data("id");
+        fetch('/admin/client/delete/'+id, {method: 'DELETE'})
+            .then(function (resp) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Nice',
+                    text: 'Suppression avec succèes',
+                })
+                tr.remove();
+            });
+    }
+});
