215af32abbfdbbb043ab4a43aa9dd98b5851ce0a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-02 22:06:55 +0100

    sd

---
+++ Website/src/Controller/Command/DeleteOrderController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Command;
+
+use App\Entity\Command;
+use App\Entity\Purchase;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DeleteOrderController extends AbstractController
+{
+    /**
+     * @Route("/command/menu/delete/{id}", name="orderDelete", methods={"GET", "DELETE"})
+     */
+    public function delete($id, EntityManagerInterface $manager){
+        if (!$this->isGranted('ROLE_TRESORIER')){
+            return $this->redirectToRoute('home');
+        }
+        /**
+         * Delete an order will delete all the purchase linked
+         */
+        $orderManager = $manager->getRepository(Command::class);
+        $purchaseManager = $manager->getRepository(Purchase::class);
+
+        $order = $orderManager->find($id);
+        $purchaseList = $purchaseManager->findBy(["command" => $order]);
+
+        foreach ($purchaseList as $purchase){
+            $manager->remove($purchase);
+        }
+        $manager->flush();
+
+        $manager->remove($order);
+        $manager->flush();
+    }
+}
--- Website/src/Controller/Command/MenuOrderController.php
+++ Website/src/Controller/Command/MenuOrderController.php
@@ -3,11 +3,9 @@
 namespace App\Controller\Command;
 
 use App\Entity\Command;
-use App\Entity\Purchase;
 use App\Manager\OrderManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -38,32 +36,5 @@ class MenuOrderController extends AbstractController
             "orderList" => $allOrder,
             "montantOrder" => $montantIdOrder
         ]);
-    }
-
-
-    /**
-     * @Route("/command/menu/delete/{id}", name="orderDelete", methods={"GET", "DELETE"})
-     */
-    public function delete($id, EntityManagerInterface $manager){
-        if (!$this->isGranted('ROLE_TRESORIER')){
-            return $this->redirectToRoute('home');
-        }
-        /**
-         * Delete an order will delete all the purchase linked
-         */
-        $orderManager = $manager->getRepository(Command::class);
-        $purchaseManager = $manager->getRepository(Purchase::class);
-
-        $order = $orderManager->find($id);
-        $purchaseList = $purchaseManager->findBy(["command" => $order]);
-
-        foreach ($purchaseList as $purchase){
-            $manager->remove($purchase);
-        }
-        $manager->flush();
-
-        $manager->remove($order);
-        $manager->flush();
-        return new JsonResponse(true);
     }
 }
