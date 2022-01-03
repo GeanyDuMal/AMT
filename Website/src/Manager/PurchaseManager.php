20cab17fe1b60dcacb289bec92e67c5cfcc96400
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-03 23:44:59 +0100

    Update PurchaseManager.php

--- Website/src/Manager/PurchaseManager.php
+++ Website/src/Manager/PurchaseManager.php
@@ -12,69 +12,11 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class PurchaseManager
 {
-    public $manager;
+    public EntityManagerInterface $manager;
 
     public function __construct(EntityManagerInterface $managerController)
     {
         $this->manager = $managerController;
-    }
-
-    /**
-     * @param $purchaseList
-     *
-     *
-     * @return void
-     */
-
-    /**
-     * @param $purchaseList [productId => quantity]
-     * @param Client|null $client Client
-     * @return array[PaymentType] $paymentTypeList
-     */
-    public function getAllowedPaymentType($purchaseList, Client $client = null): array
-    {
-        $clientTypeRepository = $this->manager->getRepository(ClientType::class);
-        if ($client != null) {
-            $clientType = $client->getClientType();
-        }else{
-            $clientType = $clientTypeRepository->findOneBy(["name" => "Etudiant"]);
-        }
-
-        $montant = 0;
-        $productRepository = $this->manager->getRepository(Product::class);
-        $paymentTypeRepository = $this->manager->getRepository(PaymentType::class);
-        $priceRepository = $this->manager->getRepository(Price::class);
-
-        foreach ($purchaseList as $productId => $quantity){
-            $product = $productRepository->find($productId);
-            $montant = $montant + $priceRepository->findOneBy(["product" => $product, "clientType" => $clientType])
-                    ->getPrice() * $quantity;
-        }
-
-        $paymentTypeList = $paymentTypeRepository->findAll();
-        foreach ($paymentTypeList as $paymentType){
-            switch ($paymentType->getName()){
-                case "Solde" :{
-                    if ($client == null || $client->getBalance() < $montant){
-                        unset($paymentTypeList[array_search($paymentType, $paymentTypeList, true)]);
-                    }
-                    break;
-                }
-                case "Carte Bancaire" :{
-                    if ($montant < 1){
-                        unset($paymentTypeList[array_search($paymentType, $paymentTypeList, true)]);
-                    }
-                    break;
-                }
-            }
-        }
-        return $paymentTypeList;
-    }
-
-    public function verifyDisponibilityProduct(Purchase $purchase): bool{
-        $product = $purchase->getProduct();
-
-        return ($product->getQuantityStock() >= $purchase->getQuantity());
     }
 
     public function persist(Purchase $purchase): void{
@@ -82,5 +24,19 @@ class PurchaseManager
             $this->manager->persist($purchase);
             $this->manager->flush();
         }
+    }
+
+    public function remove(Purchase $purchase): void{
+        $product = $purchase->getProduct();
+
+
+        $this->manager->remove($purchase);
+        $this->manager->flush();
+    }
+
+    public function verifyDisponibilityProduct(Purchase $purchase): bool{
+        $product = $purchase->getProduct();
+
+        return ($product->getQuantityStock() >= $purchase->getQuantity());
     }
 }
