537b80bdb8a1eb5a81dd0eb608183e1f19bb2976
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-03 21:40:44 +0100

    Update ajaxMenuCommand.js

--- Website/public/script/command/ajaxMenuCommand.js
+++ Website/public/script/command/ajaxMenuCommand.js
@@ -6,7 +6,6 @@ $("#order_table").on("click", "#toDelete", function(){
     if (confirm("Voulez vous vraiment supprimer cette commande ?" +
                 "Tout les achats lié a cette commandes seront supprimés")){
         var id = tr.querySelector("#idCell").innerHTML;
-        console.log(id);
         fetch('/command/menu/delete/' + id, {method: 'DELETE'})
             .then(function (resp) {
                 if (resp) {
