5c87569f367f73eb841fdecd19dc64c135037309
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-03 19:53:45 +0100

    Correction du beug d'affichage s'il n'y a qu'un seul bouton
    Corrigé avec n script, s'il n'y a qu'un seul bouton alors on ajoute du style

---
+++ Website/public/script/command/paymentOrder.js
@@ -0,0 +1,7 @@
+var form = document.querySelector(".block_button_payment");
+
+if (form.querySelectorAll("button").length === 1){
+    var button =  form.querySelector("button");
+    button.style.width = "100%";
+    button.style.padding = "10px";
+}
