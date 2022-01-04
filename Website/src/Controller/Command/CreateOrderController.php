5d2b9e405597cad4e96f49c8ed5f35e73b5dfe1b
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-04 18:47:22 +0100

    Update CreateOrderController.php

--- Website/src/Controller/Command/CreateOrderController.php
+++ Website/src/Controller/Command/CreateOrderController.php
@@ -31,7 +31,7 @@ class CreateOrderController extends AbstractController
         //Recupere tout les produits avec un stock positif afin d'afficher uniquement ceux disponibles
         $allProduct = $productRepository->findAll();
         foreach ($allProduct as $product){
-            if ($product->getQuantityStock() >0){
+            if ($product->getQuantityStock() > 0){
                 $allProductPositiveStock[] = $product;
             }
         }
