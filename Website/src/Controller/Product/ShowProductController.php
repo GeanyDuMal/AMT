5083cf903af0ada19a60d5f2772ae0ed4db863f1
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-18 18:14:37 +0100

    Update ShowProductController.php

--- Website/src/Controller/Product/ShowProductController.php
+++ Website/src/Controller/Product/ShowProductController.php
@@ -11,22 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShowProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product_list")
+     * @Route("/product{message}", name="product_list",methods={"GET", "POST"} )
      */
-    public function index(EntityManagerInterface $manager): Response
+    public function show(string $message = null,EntityManagerInterface $manager): Response
     {
-//        if (!$this->isGranted('ROLE_USER')){
-//            return $this->redirectToRoute('home');
-//        }
-        $products=$manager->getRepository(Product::class)->findAll();
-        return $this->render('product/productList.html.twig', array('products'=>$products));
-    }
-    /**
-     * @Route("/product{message}", name="product_list_message",methods={"GET", "POST"} )
-     */
-    public function show($message,EntityManagerInterface $manager): Response
-    {
-        if (!$this->isGranted('ROLE_USER')){
+        if (!$this->isGranted('ROLE_MEMBER')){
             return $this->redirectToRoute('home');
         }
         $products=$manager->getRepository(Product::class)->findAll();
