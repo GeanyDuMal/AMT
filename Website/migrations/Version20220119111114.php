c8bf107ab94e609d79b06154fcb58a42514a6912
Younes <younes.ghoniem@gmail.com>
2022-01-19 14:48:05 +0100

    trigger on Image update

--- Website/migrations/Version20220119111114.php
+++ Website/migrations/Version20220119111114.php
@@ -21,6 +21,16 @@ final class Version20220119111114 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE post CHANGE image_link image_link VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'https://a2mo-197c6.kxcdn.com/wp-content/uploads/2021/10/placeholder1.png\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('create trigger imageOnUpdate
+    before update
+    on post
+    for each row
+begin
+    IF(new.image_link is null or new.image_link=\'\') then
+        set new.image_link=\'https://a2mo-197c6.kxcdn.com/wp-content/uploads/2021/10/placeholder1.png\';
+    end if;
+end;');
+
         $this->addSql('create trigger imageOnInsert
     before insert
     on post
@@ -37,5 +47,6 @@ end;');
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE post CHANGE image_link image_link VARCHAR(255) NOT NULL');
         $this->addSql('drop trigger imageOnInsert;');
+        $this->addSql('drop trigger imageOnUpdate;');
     }
 }
