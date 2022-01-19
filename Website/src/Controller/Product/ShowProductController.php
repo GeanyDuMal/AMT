77bc8790dad7b1384f918277c31f25347c532b45
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-19 15:14:39 +0100

    remove check role show product

--- Website/src/Controller/Product/ShowProductController.php
+++ Website/src/Controller/Product/ShowProductController.php
@@ -15,10 +15,6 @@ class ShowProductController extends AbstractController
      */
     public function show(string $message = null,EntityManagerInterface $manager): Response
     {
-        if (!$this->isGranted('ROLE_ASSOC')){
-            return $this->redirectToRoute('home');
-        }
-
         $products=$manager->getRepository(Product::class)->findAll();
         return $this->render('product/productList.html.twig',[
             'products'=>$products,
