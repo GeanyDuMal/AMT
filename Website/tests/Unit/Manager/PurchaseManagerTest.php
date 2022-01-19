31a0bfdfd72880086a49f3bd8d01147e4def8d72
Younes <younes.ghoniem@gmail.com>
2022-01-19 23:02:38 +0100

    test but won't work because of entity manager

--- Website/tests/Unit/Manager/PurchaseManagerTest.php
+++ Website/tests/Unit/Manager/PurchaseManagerTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Unit\Manager;
 
+use App\Entity\Product;
+use App\Entity\Purchase;
 use App\Manager\PurchaseManager;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 
 class PurchaseManagerTest extends TestCase
@@ -10,11 +13,34 @@ class PurchaseManagerTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->product=new Product();
+        $this->purchase=new Purchase;
     }
 
 
     public function testVerifyDisponibilityProduct()
     {
+        $value=1;
+        $this->product->setName('Snickers');
+        $this->product->setQuantityStock(10);
+        $this->purchase->setProduct($this->product);
+        $this->purchase->setQuantity(5);
+
+        $purchases=$this
+            ->getMockBuilder("App\Manager\PurchaseManager")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $purchases
+            ->expects($this->any())
+            ->method('verifyDisponibiltyProduct')
+            ->with($this->purchase)
+            ->will($this->returnValue($value));
+
+        //not working yet will not continue on it today
+       // $purchaseManager=new PurchaseManager($manager);
+        //self::assertEquals(1,$purchaseManager->verifyDisponibilityProduct($this->purchase));
+
 
     }
 }
