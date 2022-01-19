73e642df77784413a4752a573fac8b24b652e49e
Younes <younes.ghoniem@gmail.com>
2022-01-19 17:14:50 +0100

    testing managers init

---
+++ Website/tests/Unit/Manager/ClientManagerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Unit\Manager;
+
+use App\Manager\ClientManager;
+use PHPUnit\Framework\TestCase;
+
+class ClientManagerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+    public function testLoginExists()
+    {
+
+
+    }
+
+    public function testVerifPassword()
+    {
+
+    }
+
+    public function testCheckMoreOneClient()
+    {
+
+    }
+
+    public function testGetRoleFromType()
+    {
+
+    }
+
+    public function testGetTypeFromRole()
+    {
+
+    }
+
+    public function testIsNotFull()
+    {
+
+    }
+
+    public function testDataCorrect()
+    {
+
+    }
+}
---
+++ Website/tests/Unit/Manager/ProductManagerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Unit\Manager;
+
+use App\Manager\ProductManager;
+use PHPUnit\Framework\TestCase;
+
+class ProductManagerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+    public function testVerifProduct()
+    {
+
+    }
+}
