12a663222c75dfbfc6a95dd5ac5c3da6cd5dd7cd
Younes <younes.ghoniem@gmail.com>
2022-01-19 12:11:57 +0100

    ProductTest.php

---
+++ Website/tests/Unit/ProductTypeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\ProductType;
+use PHPUnit\Framework\TestCase;
+
+class ProductTypeTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->productType=new ProductType();
+    }
+
+
+    public function testGetName()
+    {
+        $value='Snack';
+
+        $response=$this->productType->setName($value);
+
+        self::assertInstanceOf(ProductType::class,$response);
+        self::assertEquals($value,$this->productType->getName());
+
+    }
+}
