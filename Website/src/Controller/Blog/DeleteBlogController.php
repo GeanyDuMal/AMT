c2c2374b955690c7a213d3db4c7dcdc783abff5f
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-22 20:46:33 +0100

    editing controller response

--- Website/src/Controller/Blog/DeleteBlogController.php
+++ Website/src/Controller/Blog/DeleteBlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Blog;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -13,14 +14,17 @@ class DeleteBlogController extends AbstractController
     /**
      * @Route("/blog/delete/{id}", name="delete_blog", methods={"GET", "DELETE"})
      */
-    public function index($id,EntityManagerInterface $manager,PostRepository $postRepository){
+    public function index($id,EntityManagerInterface $manager,PostRepository $postRepository): JsonResponse
+{
         if(!$this->isGranted('ROLE_ASSOC')){
-            return $this->redirectToRoute('home');
+            return new JsonResponse(false);
         }
+
         $post=$postRepository->find($id);
 
         $manager->remove($post);
         $manager->flush();
+        return new JsonResponse(true);
     }
 
 }
