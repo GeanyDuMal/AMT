c6d0d223387b52cbc25b31f242aba0b2dffe820b
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-22 12:32:44 +0100

    Update DeleteBlogController.php

--- Website/src/Controller/Blog/DeleteBlogController.php
+++ Website/src/Controller/Blog/DeleteBlogController.php
@@ -11,13 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class DeleteBlogController extends AbstractController
 {
     /**
-     * @Route("/blog/delete{id}", name="delete_blog", methods={"GET", "DELETE"})
+     * @Route("/blog/delete/{id}", name="delete_blog", methods={"GET", "DELETE"})
      */
     public function index($id,EntityManagerInterface $manager,PostRepository $postRepository){
         if(!$this->isGranted('ROLE_ASSOC')){
             return $this->redirectToRoute('home');
         }
+        
         $post=$postRepository->find($id);
+
         $manager->remove($post);
         $manager->flush();
     }
