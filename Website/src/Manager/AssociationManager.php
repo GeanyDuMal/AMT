311aa2e52c3b40385fc26f31281317fe95de564e
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-01-31 20:46:58 +0100

    Update AssociationManager.php

--- Website/src/Manager/AssociationManager.php
+++ Website/src/Manager/AssociationManager.php
@@ -3,6 +3,7 @@
 namespace App\Manager;
 
 use App\Entity\Association;
+use App\Entity\ClientType;
 use App\Repository\ClientRepository;
 use App\Repository\ClientTypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,6 +20,40 @@ class AssociationManager
         $this->associationRepository = $this->manager->getRepository(Association::class);
     }
 
+    public function persist(Association $association){
+        $this->changeMemberTypeAdd($association);
+
+        $this->manager->persist($association);
+        $this->manager->flush();
+    }
+
+    public function remove(Association $association){
+        $this->changeMemberTypeRemove($association);
+
+        $this->manager->remove($association);
+        $this->manager->flush();
+    }
+
+    public function changeMemberTypeAdd(Association $association){
+        $client = $association->getMember();
+        $clientTypeAssociation = $this->manager->getRepository(ClientType::class)->findOneBy(["name" => "Association"]);
+
+        $client->setClientType($clientTypeAssociation);
+
+        $this->manager->persist($client);
+        $this->manager->flush();
+    }
+
+    public function changeMemberTypeRemove(Association $association){
+        $client = $association->getMember();
+        $clientTypeEtudiant = $this->manager->getRepository(ClientType::class)->findOneBy(["name" => "Etudiant"]);
+
+        $client->setClientType($clientTypeEtudiant);
+
+        $this->manager->persist($client);
+        $this->manager->flush();
+    }
+
     /**
      * Verify in the table Association if there is already a president
      * If there is one or more (which isn't possible, but it prevents bug)
@@ -30,9 +65,14 @@ class AssociationManager
         foreach ($members as $otherMember){
             if($associationMember->getMember() !== $otherMember->getMember()){
                 if($otherMember->getRole()->getName() == "President"){
+                    //Here $otherMember is the President in Function
+
                     $client = $clientRepository->findOneBy(['id'=> $otherMember->getMember()]);
+
                     $client->setClientType($clientTypeRepository->findOneBy(['name'=>'Etudiant']));
                     $client->setRoles(["ROLE_USER"]);
+
+                    $manager->remove($otherMember);
                     $manager->persist($client);
                     $manager->flush();
                 }
