b3a7c75894386e8c41d05c279b2807dbf176956e
Romain Natanelic <74205736+Spl4shh@users.noreply.github.com>
2022-06-28 13:22:33 +0200

    Ajouter le bandeau comme sur la page clientList

--- Website/src/Controller/Ordered/PaymentOrderedController.php
+++ Website/src/Controller/Ordered/PaymentOrderedController.php
@@ -112,8 +112,8 @@ class PaymentOrderedController extends AbstractController
             $orderManager->reduceBalanceIfNecessary($order);
             $orderManager->addFidelityToClient($order);
 
-            //rediriger vers l'accueil
-            return $this->redirectToRoute("orderedCreate");
+            //rediriger vers l'interface de creation de commande
+            return $this->redirectToRoute("orderedCreate", ["Commande réussie"]);
         }
 
         return $this->render('ordered/payment.html.twig', [
