048a49bfb87d142674447e58e2733d1bc1b34b1c
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-07-09 23:23:44 +0200

    modification du script pour autoriser plusieurs boutons

--- Website/public/script/generic/formGenericFunction.js
+++ Website/public/script/generic/formGenericFunction.js
@@ -1,6 +1,8 @@
 /**
- * Va desactiver le bouton lors du submit
+ * Lors du submit d'un formulaire, va desactiver tout les boutons marqués par la classe disableable
  */
 function disableButton(){
-    document.querySelector('.disableable').disabled = true
+    document.querySelectorAll('.disableable').forEach(function (button) {
+        button.disabled = true
+    })
 }
