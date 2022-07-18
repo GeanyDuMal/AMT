fab96dc72fe9bd03f3ee4fb348214d307556fffc
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-07-18 21:52:59 +0200

    Creation controller et page twig

---
+++ Website/src/Controller/ManagementController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Utils\Enum\SymfonyRole;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ManagementController extends AbstractController
+{
+    /**
+     * @Route("/management/", name="management")
+     */
+    public function index(EntityManager $manager): Response
+    {
+        if (!$this->isGranted(SymfonyRole::PRESIDENT)){
+            return $this->redirectToRoute('home');
+        }
+
+        
+
+        return $this->render('management/management.html.twig', [
+        ]);
+    }
+}
