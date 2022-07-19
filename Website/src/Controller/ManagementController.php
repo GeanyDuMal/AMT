d685213db2d5664dfd1ae308a896b06e5863111c
Romain Natanelic <74205736+Spl4shh@users.noreply.github.com>
2022-07-19 13:39:47 +0200

    Creation de l'interface premiere partie
    Creation des boutons
    Creation du CSS (à completer)
    Ajouter les zones de saisies de valeurs

--- Website/src/Controller/ManagementController.php
+++ Website/src/Controller/ManagementController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Utils\Enum\SymfonyRole;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +13,7 @@ class ManagementController extends AbstractController
     /**
      * @Route("/management/", name="management")
      */
-    public function index(EntityManager $manager): Response
+    public function index(EntityManagerInterface $manager): Response
     {
         if (!$this->isGranted(SymfonyRole::PRESIDENT)){
             return $this->redirectToRoute('home');
