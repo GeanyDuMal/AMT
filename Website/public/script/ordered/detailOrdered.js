ae84374ae4edf5a56baf1dfb0883a53f178fa876
Romain Natanelic <74205736+Spl4shh@users.noreply.github.com>
2022-07-29 10:02:28 +0200

    Update detailOrdered.js

--- Website/public/script/ordered/detailOrdered.js
+++ Website/public/script/ordered/detailOrdered.js
@@ -1,9 +1,9 @@
 function confirmDelete() {
-    return (confirm("Voulez vous vraiment supprimer cette commande ?" +
+    return (confirm("Voulez vous vraiment supprimer cette commande ? " +
         "Tout les achats liés à cette commande seront supprimés"));
 }
 
 function confirmCancel() {
-    return (confirm("Voulez vous vraiment annuler cette commande ?" +
+    return (confirm("Voulez vous vraiment annuler cette commande ? " +
         "Tout les achats liés à cette commande seront remboursés et les produits seront restockés"));
 }
