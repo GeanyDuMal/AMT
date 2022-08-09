7b6acdfa602d4ee31870a7e0c0b140a0ea4b2367
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-08-09 16:15:15 +0200

    correction

--- src/Controller/Blog/CreateBlogController.php
+++ src/Controller/Blog/CreateBlogController.php
@@ -6,6 +6,7 @@ use App\Entity\Post;
 use App\Manager\PostManager;
 use App\Repository\PostRepository;
 use App\Utils\Enum\PostType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,7 +34,7 @@ class CreateBlogController extends AbstractController
         $message = "";
 
         if($data->count() > 0){
-            $postmanager->setData($post, $data->get('postType'), $data->get("postTitle"), $data->get('postDescription'), $data->get('imageLink'));
+            $postmanager->setData($post, $data->get('postType'), $data->get("postTitle"), $data->get('postDescription'), $data->get('imageLink'), new DateTime("now"));
 
             if($postmanager->verifyPost($post)){
                 if($postRepository->findBy(['title' => $post->getTitle()])){
