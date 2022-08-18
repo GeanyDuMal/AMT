966d02451d3b8449ecf34a06152fa9ac1b0047c0
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-08-18 22:36:10 +0200

    Creation de l'entity PasswordForgotRequest

---
+++ src/Entity/PasswordForgotRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PasswordForgotRequestRepository;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PasswordForgotRequestRepository::class)
+ */
+class PasswordForgotRequest
+{
+    /**
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity=Client::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private Client $client;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private DateTimeInterface $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $confirmationCode;
+
+
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(Client $client): self
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getDate(): DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getConfirmationCode(): string
+    {
+        return $this->confirmationCode;
+    }
+
+    public function setConfirmationCode(string $confirmationCode): self
+    {
+        $this->confirmationCode = $confirmationCode;
+
+        return $this;
+    }
+}
