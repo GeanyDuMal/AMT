02ed367b7e941e29c10d543c5ecb4459badaf179
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-10-07 13:52:26 +0200

    ajout du script avec la class pour le css
    Voir si possible de mettre une horloge pour changer d'image toute les X secondes

--- public/script/home/carousel.js
+++ public/script/home/carousel.js
@@ -1,14 +1,40 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
-});
+const slides = document.querySelectorAll(".slide");
+const slidesQuantity = slides.length;
 
-prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
-});
+function switchCarouselItem() {
+    let active = document.querySelector(".slide-active");
+    let indexActive;
+
+    /**
+     * Deprecated
+     * Fonctionnel mais a remplacer une fois une solution trouvé
+     */
+    let sourceButton = event.target;
+
+    for (let i = 0; i < slidesQuantity; i++) {
+        if (slides.item(i) === active){
+            indexActive = i;
+        }
+    }
+    
+    if (sourceButton === nextButton) {
+        if (indexActive === slidesQuantity - 1) {
+            slides.item(0).classList.add("slide-active")
+        } else {
+            slides.item(indexActive + 1).classList.add("slide-active")
+        }
+    } else if (sourceButton === prevButton) {
+        if (indexActive === 0){
+            slides.item(slidesQuantity-1).classList.add("slide-active")
+        } else {
+            slides.item(indexActive -1).classList.add("slide-active")
+        }
+    } else {
+        console.log("Bouton non existant")
+    }
+
+    active.classList.remove("slide-active");
+}
