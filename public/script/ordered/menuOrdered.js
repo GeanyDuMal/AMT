1be8ab52817cc4bcf8e20a37b7c8f55f7c9818bc
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2023-01-23 21:52:19 +0100

    Menu Ordered

--- public/script/ordered/menuOrdered.js
+++ public/script/ordered/menuOrdered.js
@@ -5,7 +5,7 @@
  * sorting per column
  */
 $(document).ready( function () {
-    $('#ordered_table').DataTable(
+    $('#ordered-table').DataTable(
         {
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json"
