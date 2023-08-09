e92c0d5ba5e0a7175c06beef2400fe94564cef10
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2023-08-09 23:44:58 +0200

    Ajout de parameter aux pages client

--- src/Controller/Client/ShowClientController.php
+++ src/Controller/Client/ShowClientController.php
@@ -2,29 +2,34 @@
 
 namespace App\Controller\Client;
 
-use App\Repository\MemberRepository;
+use App\Manager\ParameterManager;
 use App\Repository\ClientRepository;
+use App\Repository\MemberRepository;
 use App\Utils\Enum\ClientType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
 class ShowClientController extends AbstractController
 {
     /**
      * @Route("/admin/client/show/{!id}", name="showClient")
      */
-    public function index($id, ClientRepository $clientRepository, MemberRepository $memberRepository): Response
-    {
+    public function index($id, ClientRepository $clientRepository, MemberRepository $memberRepository, EntityManagerInterface $manager): Response {
+        $parameterManager = new ParameterManager($manager);
+        $parameter = $parameterManager->getParameter();
         $client = $clientRepository->find($id);
 
         if ($client) {
             $member = $memberRepository->findOneBy(["client" => $client]);
 
             return $this->render('client/ShowClient.html.twig', [
-                'client' => $client,
-                'clientTypes' => ClientType::getAll(),
-                'member' => $member,
+                "parameter" => $parameter,
+                "client" => $client,
+                "clientTypes" => ClientType::getAll(),
+                "member" => $member,
             ]);
         } else {
             return $this->redirectToRoute('menuClient');
