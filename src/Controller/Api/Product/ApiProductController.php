40892445122ec413e572cf66acd3f5879189f613
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2023-09-23 14:41:04 +0200

    Correction suite a mise en prod

--- src/Controller/Api/Product/ApiProductController.php
+++ src/Controller/Api/Product/ApiProductController.php
@@ -13,25 +13,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiProductController extends AbstractController
 {
-    private ClientManager $clientManager;
-
     /**
      * @Route("/api/product/getAll", name="apiProductGetAll")
      */
     public function index(EntityManagerInterface $manager): JsonResponse {
-        $this->clientManager = new ClientManager($manager);
         $productManager = new ProductManager($manager);
 
         return $this->json($productManager->getAllProductAvailable(), Response::HTTP_OK, [], ["groups" => ["product", "price"]]);
-    }
-
-    private function checkAuthenticate(?string $login, ?string $password): bool {
-        if (!$login || !$password) {
-            return false;
-        }
-
-        $client = $this->clientManager->getClientByLogin($login);
-
-        return ($client && password_verify($password, $client->getPassword()));
     }
 }
