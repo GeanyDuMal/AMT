d8d372bea28138ff236cbf9349ce4d6b00ab360c
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2023-09-28 13:39:14 +0200

    correction front

--- public/script/ordered/paymentOrdered.js
+++ public/script/ordered/paymentOrdered.js
@@ -1,3 +1,12 @@
 function preventPayment() {
     return confirm("Attention, toute commande effectuée ne pourras etre remboursée, etes vous sûr des elements que vous avez saisi ?")
 }
+
+function disableButtonPaymentOrdered() {
+    let buttons = document.querySelectorAll('.btn')
+    let divOrderedPending = document.querySelector('#pendingOrdered')
+    buttons.forEach(button => {
+        button.style.display = "none";
+    })
+    divOrderedPending.style.display = "block";
+}
